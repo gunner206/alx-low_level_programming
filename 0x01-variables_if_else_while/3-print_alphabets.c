2614715807cdf7217c2ea005787db149f6faa425
gunner206 <gunner.alx@gmail.com>
2023-03-17 15:28:16 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+	char x;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (x = 'A' ; x <= 'Z' ; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}

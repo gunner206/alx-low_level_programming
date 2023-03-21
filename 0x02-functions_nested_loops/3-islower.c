05865cba7f319dbffcc4ae69cad8e6dbeeea81d0
gunner206 <gunner.alx@gmail.com>
2023-03-21 14:55:51 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - shearch for a lower case alphabete
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}

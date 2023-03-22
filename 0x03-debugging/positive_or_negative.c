09aaf6407a87bf89944add341f813f316ef482ac
gunner206 <gunner.alx@gmail.com>
2023-03-22 15:42:00 -0700

    0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,8 +9,6 @@ void positive_or_negative(int i)
 {
 	if (i < 0)
 		printf("%d is negative\n", i);
-	else if (i > 0)
+	else
 		printf("%d is positive\n", i);
-	else if (i == 0)
-		printf("%d is zero\n", i);
 }

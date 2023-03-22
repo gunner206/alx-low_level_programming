0300dcad23b02df8abd6ebf2bfe7fb8c330c3a91
gunner206 <gunner.alx@gmail.com>
2023-03-21 17:47:22 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add to integers
+ * Return: sum
+ * @a: intrepeter
+ * @b: intrepeter
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	_putchar(sum + '0');
+	return (sum);
+}

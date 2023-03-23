1d68d8e6bc33ca62c0917e0f4bf69fdca5c6aad7
gunner206 <gunner.alx@gmail.com>
2023-03-23 07:11:12 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print_numbers
+ * Return: void
+ * _putchar: print alphabetes
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}

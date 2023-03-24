e73a21f03e96b00549effcbab17a1d52f84c2405
gunner206 <gunner.alx@gmail.com>
2023-03-23 17:58:04 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if ((i % 3) == 0)
+			printf("fizz ");
+		else if ((i % 5) == 0)
+			printf("buzz ");
+		else if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}

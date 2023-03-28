ce214e6bfcc7f972f25f5201f2506b5808101381
gunner206 <gunner.alx@gmail.com>
2023-03-28 08:19:31 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in revers
+ * @s: a string to print
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	for(j = i; j >= 0 ;j--)
+	{
+		_putchar(*s--);
+	}
+	_putchar('\n');
+}

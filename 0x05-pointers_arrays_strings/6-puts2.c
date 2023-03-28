bd2b259cc004f91e758db45d6efef9ae399e29f1
gunner206 <gunner.alx@gmail.com>
2023-03-28 13:25:30 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,7 +10,10 @@ void puts2(char *str)
 
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		_putchar(str[i]);
+		if (str[i] == '\0')
+			_putchar('\n');
+		else
+			 _putchar(str[i]);
 		i++;
 	}
 	_putchar('\n');

3ed238a870f78a2f9c591ea2ec15c20383747062
gunner206 <gunner.alx@gmail.com>
2023-03-28 11:17:23 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second halp of a string
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+		length++;
+	if (length % 2 == 0)
+	{
+		for (i = length / 2 ; str[i] != '\0' ; i++)
+			_putchar(str[i]);
+	}
+	else if (length % 2 != 0)
+	{
+		for (i = (length - 1) / 2 ; str[i] != '\0' ; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}

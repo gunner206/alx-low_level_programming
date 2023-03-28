9c8ad9674e3425e83b8a2beb08122aba049e1c65
gunner206 <gunner.alx@gmail.com>
2023-03-28 13:46:31 -0700

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,18 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int l = 0;
+	int x = 0;
 
-	for (i = 0 ; src[i] != '\0' ; i++)
-		dest[i] = src[i];
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
 	return (dest);
+
 }

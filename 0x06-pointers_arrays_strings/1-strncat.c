d62c3e2a8f50677214ed42d717797fd5b7df389d
gunner206 <gunner.alx@gmail.com>
2023-03-30 18:04:17 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,18 +8,39 @@
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
+/*
+ * {
+ *	int srclen = 0;
+ *	int destlen = 0;
+ *	int i;
+ *
+ *	for (i = 0 ; dest[i] != '\0' ; i++)
+ *		destlen++;
+ *	for (i = 0 ; src[i] != '\0' ; j++)
+ *		srclen++;
+ *	for (i = 0; i < n ; i++)
+ *		dest[destlen + i] = src[i];
+ *	return (dest);
+ *
+ * }
+ */
 {
-	int srclen = 0;
-	int destlen = 0;
 	int i;
+	int j;
 
-	for (i = 0 ; dest[i] != '\0' ; i++)
-		destlen++;
-	for (i = 0 ; src[i] != '\0' ; i++)
-		srclen++;
-	for (i = 0 ; i < n ; i++)
-		dest[destlen + i] = src[i];
-	for(; i < n ; i++)
-		dest[i] = '\0';
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
+

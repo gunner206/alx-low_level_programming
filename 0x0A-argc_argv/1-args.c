6da3cbc0656a8e1c6f5eb7f17c9d29133e152fa6
gunner206 <gunner.alx@gmail.com>
2023-04-15 10:18:41 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: list
+ * Return: always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}

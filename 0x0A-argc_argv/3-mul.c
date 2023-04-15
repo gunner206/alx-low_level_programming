88a72a64a99afffb4f9804739bea229e5dfd1529
gunner206 <gunner.alx@gmail.com>
2023-04-15 10:48:53 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: lisr
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+	return (1);
+	}
+	else
+	{
+		mul  = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+	}
+	return (0);
+}

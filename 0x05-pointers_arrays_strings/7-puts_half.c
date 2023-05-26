5a840a869cea73f9d769185e1acb81f6f8afe4f7
gunner206 <gunner.alx@gmail.com>
2023-05-26 03:35:55 -0700

    1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,32 +4,33 @@
  * puts_half - prints the second halp of a string
  * @str: string to be printed
  */
+
 void puts_half(char *str)
 {
-        int length = 0;
-        int i;
-        int n;
-        char *y = str;
+	int length = 0;
+	int i;
+	int n;
+	char *y = str;
 
-        while (*y != '\0')
-        {
-                y++;
-                length++;
-        }
-        n = length / 2;
-        for (i = n ; i < length ; i++)
-        {
-                if (length % 2 == 0)
-                {
-                        n = length / 2;
-                        _putchar(str[i]);
-                }
-                else if (length % 2 == 1)
-                {
-                        n = (length + 1) / 2;
-                        _putchar(str[i]);
-                }
-        }
-        _putchar('\n');
+	while (*y != '\0')
+	{
+		y++;
+		length++;
+	}
+	n = length / 2;
+	for (i = n ; i < length ; i++)
+	{
+		if (length % 2 == 0)
+		{
+			n = length / 2;
+			_putchar(str[i]);
+		}
+		else if (length % 2 == 1)
+		{
+			n = ((length + 1) / 2);
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
 }
 

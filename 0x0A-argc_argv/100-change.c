1011e39841f697d272f71a19f85bee9904eb8477
gunner206 <gunner.alx@gmail.com>
2023-06-11 07:28:22 -0700

    5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: always o
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int money = atoi(argv[1]);
+	int cents[] = {25, 10, 5, 2, 1};
+	int leastcents = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 0 ; i < 5 ; i++)
+	{
+		if (money >= cents[i])
+		{
+			leastcents += money / cents[i];
+			money = money % cents[i];
+			if (money % cents[i] == 0)
+			{
+				break;
+			}
+		}
+	}
+	printf("%d\n", leastcents);
+	return (0);
+}

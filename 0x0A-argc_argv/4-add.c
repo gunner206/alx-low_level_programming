83b6ddd20e8473b1f5da2c3227df4eb47f82d884
gunner206 <gunner.alx@gmail.com>
2023-06-11 07:01:05 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - it all start here
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc] ; *c ; c++)
+			if (*c < '0' || *c > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}

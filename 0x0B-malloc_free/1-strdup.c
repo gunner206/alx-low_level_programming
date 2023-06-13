6933d46809397897d48464af252c81212040590a
gunner206 <gunner.alx@gmail.com>
2023-06-13 07:24:37 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strdup - dupplicate a string
+ * @str: string
+ * Return: uplicated string
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int length = 0;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0 ; str[i] != '\0' ; i++)
+		length++;
+
+	s = malloc(sizeof(char) * length);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < length ; i++)
+	{
+		s[i] = str[i];
+	}
+	return (s);
+}

abe5c2d8e462ac785bf216e606caa086f540ea7e
gunner206 <gunner.alx@gmail.com>
2023-06-14 07:06:26 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 	int s2len = 0;
 	int size;
 
-	if (s1 == NULL || s2 == NULL)
-		return ('\0');
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0 ; s1[i] != '\0' ; i++)
 		s1len++;

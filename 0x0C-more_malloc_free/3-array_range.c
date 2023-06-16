1faa8a24220494149eb578cf37d482b00b48013c
gunner206 <gunner.alx@gmail.com>
2023-06-16 03:44:11 -0700

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	for (i = 0 ; i < max ; i++)
-		arr[i] = min + i;
+	for (i = 0 ; min <= max ; i++)
+		arr[i] = min++;
 	return (arr);
 }

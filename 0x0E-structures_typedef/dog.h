7536e65893a97235d438cca158329cc07477380c
gunner206 <gunner.alx@gmail.com>
2023-06-19 15:57:43 -0700

    2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,8 @@ struct dog
 	float age;
 	char *owner;
 };
-
+#include <stdio.h>
+#include <stdlib.h>
+void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif

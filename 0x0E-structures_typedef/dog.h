41938d582486b457c668729880931ac950a66b52
gunner206 <gunner.alx@gmail.com>
2023-06-19 16:04:06 -0700

    3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,17 +3,18 @@
 
 
 /**
- * struct dog - dog info
+ * dog_t - dog info
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
+
 #include <stdio.h>
 #include <stdlib.h>
 void print_dog(struct dog *d);

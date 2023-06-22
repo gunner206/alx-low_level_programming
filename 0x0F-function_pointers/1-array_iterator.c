f6cb6effabe53d2d167d0671679d736727996f46
gunner206 <gunner.alx@gmail.com>
2023-06-22 02:47:05 -0700

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execites fun in each element of arr
+ * @array: array elements
+ * @size: size of array
+ * action: func to be executed
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		action(array[i]);
+	}
+}

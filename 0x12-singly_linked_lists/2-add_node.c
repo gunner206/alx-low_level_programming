153f76775a5c09084ebf4e0667d1f70cb15a0f18
gunner206 <gunner.alx@hmail.com>
2023-07-01 14:38:45 -0700

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,18 +9,17 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *s = strdup(str);
 	int i;
 	unsigned int len = 0;
 	list_t *N;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
+	for (i = 0 ; str[i] != '\0' ; i++)
 		len++;
 
 	N = malloc(sizeof(list_t));
 	if (!N)
 		return (NULL);
-	N->str = s;
+	N->str = strdup(str);
 	N->len = len;
 	N->next = *head;
 	*head = N;

b11bcaf448f9444a497e56040842c9821826024b
gunner206 <gunner.alx@hmail.com>
2023-07-01 03:45:59 -0700

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: node
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t sum = 0;
+
+	while (h != NULL)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		sum++;
+	}
+	return (sum);
+}

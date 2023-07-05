d317a7b8d691a134803baaf6116f681e1fe43e55
gunner206 <gunner.alx@hmail.com>
2023-07-05 09:32:37 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	node = malloc(sizeof(listint_t));
-	if (!node || !*head)
+	if (!node || !head)
 		return (NULL);
 
 	node->n = n;

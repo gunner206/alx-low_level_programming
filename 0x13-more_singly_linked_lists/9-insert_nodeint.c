5b47f372fd3568a3bcc7ba6a6c6d27161621ec7c
gunner206 <gunner.alx@hmail.com>
2023-07-05 09:27:14 -0700

    9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert a node at given index
+ * @idx: index
+ * @head: node head
+ * @n: node data
+ *
+ * Return: adress of node if fail null
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp = *head;
+	listint_t *node;
+	unsigned int i = 0;
+
+	node = malloc(sizeof(listint_t));
+	if (!node || !*head)
+		return (NULL);
+
+	node->n = n;
+	node->next = NULL;
+
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+
+	while (temp && i < idx)
+	{
+		if (i == idx - 1)
+		{
+			node->next = temp->next;
+			temp->next = node;
+			return (node);
+		}
+		else
+			temp = temp->next;
+		i++;
+	}
+	return (NULL);
+}

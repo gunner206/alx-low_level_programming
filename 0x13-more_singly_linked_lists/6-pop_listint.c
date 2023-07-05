9f51bc31fc294f38425d7b25885d8be4e9f19f04
gunner206 <gunner.alx@hmail.com>
2023-07-05 08:16:52 -0700

    6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,14 +8,15 @@
  */
 int pop_listint(listint_t **head)
 {
+	int data;
 	listint_t *temp;
 
 	if (!head || !*head)
 		return (0);
-
+	data = (*head)->n;
 	temp = (*head)->next;
 	free(*head);
 	(*head) = temp;
 
-	return ((*head)->n);
+	return (data);
 }

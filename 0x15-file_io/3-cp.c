13f5a993d28ece0da1417854ba35bbc43e8df46f
gunner206 <gunner.alx@hmail.com>
2023-07-11 16:15:09 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,30 +1,9 @@
 #include "main.h"
 
-char *create_buffer(char *file);
 void close_file(int fd);
 
+#define BUFFER_SIZE 1024
 
-/**
- * create_buffer - Allocates 1024 bytes
- * @file: name of file
- *
- * Return: pointer to new buffer
- */
-char *create_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
-}
 /**
  * close_file - a fonction that closes file
  * @fd: file descriptor
@@ -35,7 +14,7 @@ void close_file(int fd)
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't close fd %d", fd);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -54,16 +33,15 @@ int main(int argc, char *argv[])
 	int fd2;
 	int r;
 	int w;
-	char *buffer;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = create_buffer(argv[2]);
 	fd1 = open(file_from, O_RDONLY);
-	r = read(fd1, buffer, 1024);
+	r = read(fd1, buffer, BUFFER_SIZE);
 	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	do {
 		if (fd1 == -1 || r == -1)
@@ -75,14 +53,12 @@ int main(int argc, char *argv[])
 		if (fd2 == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buffer);
 			exit(99);
 		}
-		r = read(fd1, buffer, 1024);
+		r = read(fd1, buffer, BUFFER_SIZE);
 		fd2 = open(file_to, O_WRONLY | O_APPEND);
 	} while (r > 0);
 
-	free(buffer);
 	close_file(fd1);
 	close_file(fd2);
 	return (0);
